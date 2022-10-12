b5180c11909e3059ef611740342a03aba32d04ce
strick <strick@yak.net>
2022-10-12 01:24:20 -0700

    nullboot.wav demo works.

---
+++ frob2/bootrom/nullboot.c
@@ -0,0 +1,23 @@
+// nullboot.c
+//
+// A demo program to do nothing but write a distinctive
+// pattern on the screen as Proof Of Concept, and return.
+
+// Hints:
+//   Linux:
+//     make nullboot.wav
+//   Coco:
+//     CLOADM"NULLBOOT
+//   Linux:
+//     aplay nullboot.wav
+//   Coco:
+//     EXEC &H2600
+
+void main () {
+    // Poke alphabet over the screen, with stride 3,
+    // skipping the first screen line of 32 bytes.
+    for (unsigned p = 0x0420; p < 0x0600; p++) {
+        // Use 64-char alphabet. Use inverted colors.
+        *(char*)p = 0x40 | (0x3f & (char)(p+p+p));
+    }
+}
--- frob2/bootrom/preboot.asm
+++ frob2/bootrom/preboot.asm
@@ -1,13 +1,68 @@
+*** frob2/netboot/preboot.asm
+***
+*** The purpose of this code is to disable interrupts,
+*** choose a Stack region, and call the C main() function.
+*** It replaces the ordinary "cstart" code,
+*** for use in a Boot ROM.
+***
+*** Assumes VDG is in default initialization with screen
+*** buffer at $0400.  This code uses no ROM calls.
+*** The code is relocatable.  Regardless where you load it,
+*** jump to (or Basic `EXEC`) its first byte to execute it.
+***
+*** First prints a banner at the top of the screen.
+*** Then calls _main.
+*** It is not intended that the main function ever returns.
+*** But if _main returns, print the banner backwards across the
+*** middle of the screen, and get stuck.
+***
+*** You can CLOADM and EXEC a binary to test it.
+*** But it is intended that this will eventually call
+*** the main() routine for the netboot ROM.
+
     IMPORT program_start
     IMPORT _main
 
     SECTION start
 
-    lds #$2000
-    ldy #$0000
-    ldu #$0000
+preboot
+    orcc #$50   ; disable interrupts
+    lds #$0400  ; 1KB stack in lowest memory
+
+    clrb
+    tfr b,dp    ; dp=0
+
+    ldy #$0000  ; no global vars
+    ldu #$0000  ; no previous frame
+
+*** Display an ID at top of screen, to show preboot is running.
+    leax id_string,pcr
+    ldy #$0400
+id_loop
+    lda ,x
+    anda #63
+    sta ,y+
+    lda ,x+
+    bpl id_loop  ; while no N bit
+
+    pshs y,u    ; 8 bytes of zeros onto stack
     pshs y,u
-    pshs y,u
-    jmp _main
+    lbsr _main  ; and call main with no args, which never returns.
+
+*** Show id backwards if main returns, and get stuck.
+not_reached
+    orcc #$50   ; disable interrupts
+    leax id_string,pcr
+    ldy #$0500  ; middle of screen
+di_loop
+    lda ,x
+    anda #63
+    sta ,-y
+    lda ,x+
+    bpl di_loop
+di_stuck
+    bra di_stuck
+
+id_string  fcs / -- STRICKYAK FROBIO PREBOOT -- /
 
     ENDSECTION
