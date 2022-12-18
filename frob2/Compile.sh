b9149e7c737964ac67b336c10bdeb037f2c8f897
strick <strick@yak.net>
2022-12-17 16:58:31 -0800

    honor environment MAX_VERBOSITY

--- frob2/Compile.sh
+++ frob2/Compile.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #
 # Usage:
-#    sh Compile object sources...
+#    HOW=... MAX_VERBOSE=9 sh Compile object sources...
 
 OBJECT="$(basename $1 .os9)"
 shift
 
+MAX_VERBOSE=${MAX_VERBOSE:-9}
+
 set -ex
 case $HOW in
     "" | cmoc )
-        cmoc -i --os9 -I.. -DMAX_VERBOSE=9 -o "$OBJECT" "$@"
+        cmoc -i --os9 -I.. -DMAX_VERBOSE="$MAX_VERBOSE" -o "$OBJECT" "$@"
         mv "$OBJECT" "$OBJECT.os9"
         os9 ident "$OBJECT.os9"
         ;;
     cmocly )
-        /home/strick/go/bin/cmocly --cmoc_pre="-DMAX_VERBOSE=9" -I=.. --o "$OBJECT" "$@" &&
+        /home/strick/go/bin/cmocly --cmoc_pre="-DMAX_VERBOSE=$MAX_VERBOSE" -I=.. --o "$OBJECT" "$@" &&
         mv "$OBJECT" "$OBJECT.os9"
         os9 ident "$OBJECT.os9"
         ;;
