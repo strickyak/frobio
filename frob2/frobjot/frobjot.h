64f848150138768688ae9c4c3bd407b1781a0642
strick <strick@yak.net>
2023-02-09 00:07:22 -0800

    add unused Jot, and an extra stdlib

---
+++ frob2/frobjot/frobjot.h
@@ -0,0 +1,121 @@
+// Jot: small (62 bytes max) ASCII strings.
+//
+// Jots require exactly 64 bytes, because they
+// start with a length byte, and end with a '\0'.
+//
+// If the Jot overflows, its final character
+// is changed to '#'.
+//
+// Jots only contain nice characters, which are
+// CR, Space, and the 95 printable ASCII chars.
+// If NL is appended, it turns into CR.
+// If other characters are appended, they turn
+// into \ooo octal escapes strings.
+
+#include "frob2/froblib.h"
+
+#define JOT_MAX 62
+
+typedef struct jot {
+  byte len; 
+  char s[JOT_MAX+1];
+} Jot;
+
+const byte HexAlphabet[] = "0123456789ABCDEF";
+
+bool JotNiceCharP(char c) {
+    return (' ' <= c && c <= '~' || c=='\r');
+}
+
+#if 0
+void JotCheck(Jot* p) {
+  Assert(p->len <= JOT_MAX);
+  Assert(p->s[p->len] == '\0');
+  for (byte i=0; i<p->len; i++) {
+    char c = p->s[i];
+    Assert(JotNiceCharP(c));
+  }
+}
+#endif
+
+// To initialize a Jot: struct j62 = {0};
+//
+
+void JotAppC(Jot* j, char c) {
+  if (c=='\n') c='\r';
+  if (JotNiceCharP(c)) {
+    if (j->len < JOT_MAX) {
+      j->s[j->len++] = c;
+      j->s[j->len] = '\0';
+    } else {
+      j->s[JOT_MAX-1] = '#'; // Overflow char.
+    }
+  } else {
+    JotAppC(j, '\\');
+    byte b = (byte)c;
+    byte b3 = b>>3;
+    JotAppC(j, '0'+ (7 & (b3>>3)));
+    JotAppC(j, '0'+ (7 & (b3)));
+    JotAppC(j, '0'+ (7 & b));
+  }
+}
+
+void JotAppU(Jot* j, word x) {
+  if (x > 9) {
+    JotAppU(j, x/10);
+  }
+  JotAppC(j, '0' + (char)(x % 10));
+}
+
+void JotAppX(Jot* j, word x) {
+  if (x > 15) {
+    JotAppX(j, x>>4);
+  }
+  JotAppC(j,  HexAlphabet[ x & 15 ]);
+}
+
+void JotAppS(Jot* j, const char* x) {
+  while (*x)
+    JotAppC(j, *x++);
+}
+
+void JotAppJot(Jot* j, const Jot* x) {
+  for (byte i = 0; i < x->len; i++)
+    JotAppC(j, x->s[i]);
+}
+
+void JotFormatVA(Jot* j, const char* format, va_list ap) {
+  for (const char* s = format; *s; s++) {
+    if (*s != '%') {
+      JotAppC(j, *s);
+      continue;
+    }
+    s++;
+    switch (*s) {
+      case 'x': 
+        // word x = va_arg(ap, word);
+        JotAppX(j, va_arg(ap, word));
+               
+        break;
+
+      case 'u': 
+        // word u = va_arg(ap, word);
+        JotAppU(j, va_arg(ap, word));
+               
+        break;
+
+      case 's': 
+        // const char* s = va_arg(ap, const char*);
+        JotAppS(j, va_arg(ap, const char*));
+               
+        break;
+    }
+  }
+}
+
+void JotPrintf(Jot* j, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    JotFormatVA(j, format, ap);
+    va_end(ap);
+}
---
+++ frob2/frobjot/stdlib.c
@@ -0,0 +1,32 @@
+void abort() {
+  // Assume text screen is still at 0x0400,
+  // and splash the middle of it with Xs.
+  for (int p=0x04C0; p < 0x0540; p++) {
+    *(char*)p = 'X' & 63;
+  }
+  while (1) {}
+}
+
+void memcpy(char* a, const char* b, int c) {
+  for (int i = 0; i < c; i++) *a++ = *b++;
+}
+
+void memset(char* a, int b, int c) {
+  for (int i = 0; i < c; i++) *a++ = b;
+}
+void strcpy(char* a, const char*b) {
+  while (*b) *a++ = *b++;
+}
+void strncpy(char* a, const char*b, int n) {
+  int i = 0;
+  while (*b) {
+    *a++ = *b++;
+    i++;
+    if (i>=n) break;
+  }
+}
+int strlen(const char* s) {
+  const char* p = s;
+  while (*p) p++;
+  return p-s;
+}
