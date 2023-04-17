eb9e75b663a1e80870b74bd263f3bf542677f1eb
strick <strick@yak.net>
2023-04-17 02:07:59 -0700

    slotting future

--- frob2/waiter/cli/server.go
+++ frob2/waiter/cli/server.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 	"github.com/strickyak/frobio/frob2/waiter"
 	_ "github.com/strickyak/frobio/frob2/waiter/canvas"
+	"github.com/strickyak/frobio/frob2/waiter/future"
 )
 
 func main() {
 	flag.Parse()
 	go waiter.Listen()
+	future.Run()
 	waiter.Console()
 }
