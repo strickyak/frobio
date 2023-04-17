1df635146543a4b0a355d6f1895856c22b61fb71
strick <strick@yak.net>
2023-04-17 02:06:22 -0700

    future/con.go

--- frob2/waiter/future/con.go
+++ frob2/waiter/future/con.go
@@ -1,10 +1,18 @@
 package lemma
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
+
+	. "github.com/strickyak/frobio/frob2/waiter"
 )
 
+/*
 const (
 	POKE = 0
 	CALL = 255
@@ -19,10 +27,11 @@ func Hi(x int) byte {
 func Lo(x int) byte {
 	return 255 & byte(uint(x))
 }
+*/
 
 type Quint [5]byte // Quintabyte commands.
 
-func NewQuint(cmd byte, n int, p int) Quint {
+func NewQuint(cmd byte, n uint, p uint) Quint {
 	var q Quint
 	q[0] = cmd
 	q[1] = Hi(n)
@@ -35,10 +44,10 @@ func NewQuint(cmd byte, n int, p int) Quint {
 func (q Quint) Command() byte {
 	return q[0]
 }
-func (q Quint) N() int {
+func (q Quint) N() uint {
 	return HiLo(q[1], q[2])
 }
-func (q Quint) P() int {
+func (q Quint) P() uint {
 	return HiLo(q[3], q[4])
 }
 
@@ -54,7 +63,7 @@ func (ses *Session) SendQuint(q Quint) {
 
 func (ses *Session) RecvQuint() Quint {
 	var q Quint
-	cc, err := ses.Conn.ReadFull(q[:])
+	cc, err := io.ReadFull(ses.Conn, q[:])
 	if err != nil {
 		log.Panicf("RecvQuint(%q) got err: %v", ses.ID, err)
 	}
@@ -74,7 +83,7 @@ type Screen interface {
 	Redraw()
 }
 
-type Text struct {
+type TextScreen struct {
 	Old  []byte // screen RAM already sent.
 	New  []byte // screen RAM not yet sent.
 	W, H int    // screen dimensions
@@ -82,15 +91,15 @@ type Text struct {
 	Addr int
 }
 
-func NewText(w, h int, addr int) *Text {
+func NewTextScreen(w, h int, addr int) *TextScreen {
 	o := make([]byte, w*h)
 	n := make([]byte, w*h)
-	for i := 0; i < w_h; i++ {
+	for i := 0; i < w*h; i++ {
 		o[i] = ' '
 		n[i] = ' '
 	}
 
-	return &Text{
+	return &TextScreen{
 		Old:  o,
 		New:  n,
 		W:    w,
@@ -101,8 +110,14 @@ func NewText(w, h int, addr int) *Text {
 	}
 }
 
-func (t *Text) Clear() {
-	t.Screen = make([]byte, t.W*t.H)
+func (t *TextScreen) Clear() {
+	t.New = make([]byte, t.W*t.H)
+	for i := range t.New {
+		t.New[i] = 32
+	}
+}
+func (t *TextScreen) PutChar(ch byte) {
+	TODO()
 }
 
 type LineBuf struct {
@@ -111,69 +126,79 @@ type LineBuf struct {
 
 type Session struct {
 	ID      string
-	Conn    *net.Conn
-	Screen  *Screen
+	Conn    net.Conn
+	Screen  Screen
 	LineBuf *LineBuf
-	Env     map[string]string // for whatever
-	User    *User             // logged in user
-	Card    Card              // Current card.
-}
-
-func (ses *Session) GoTo(card Card) {
-	ses.Card = card
-	card.Activate(ses)
+	// Env     map[string]string // for whatever
+	// User    *User             // logged in user
+	Card Card // Current card.
 }
 
 // Run() is called with "go" to manage a connection session.
-var nextID int = 101
+var nextID int = 1001
 
-func Run(conn *net.Conn, initialCard Card) {
+func NewSession(conn net.Conn, initialCard Card) *Session {
 	ses := &Session{
 		ID:      fmt.Sprintf("ses%d", nextID),
 		Conn:    conn,
-		Screen:  NewScreen(),
+		Screen:  NewTextScreen(32, 16, 0x0400),
 		LineBuf: nil,
 	}
 	nextID++
 
-	ses.GoTo(initialCard)
-
-	for {
-		q := ses.RecvQuint()
-		switch q.Command() {
-		case INKEY:
-			ses.LineBuf.Inkey(q.P())
-		default:
-			fmt.Panicf("Unexpected RecvQuint Command: %d", q.Command())
+	/*
+		for {
+			q := ses.RecvQuint()
+			switch q.Command() {
+			case CMD_INKEY:
+				ses.LineBuf.Inkey(q.P())
+			default:
+				fmt.Panicf("Unexpected RecvQuint Command: %d", q.Command())
+			}
 		}
-	}
+	*/
+	return ses
 }
 
 type Card interface {
-	Activate(ses *Session)
-	// Accept(ses *Session, submission string) Card
+	// Activate(ses *Session)
+	Name() string
 }
 
-type PromptCard struct {
-	Prompt  string
-	Actions map[string]*Card
+var Cards = make(map[int]Card)
+
+type TCard struct {
+	Num  int
+	name string
+	Moms []int
+	Kids map[int]Card
+
+	Text    string
+	Actions map[string]Card
 }
 
-func (pc *PromptCard) Activate(ses *Session) {
+/*
+func (tc *TCard) Activate(ses *Session) {
 	ses.Screen.Clear()
-	ses.Screen.PutStr(pc.Prompt)
+	ses.Screen.PutStr(tc.Text)
+	for num, kid := range tc.Kids {
+		ses.Screen.PutStr(fmt.Sprintf("[%d] %s\n", num, kid.name))
+	}
 	ses.Screen.Push(ses)
-	ses.LineHandler = func() {
-		s := string(ses.LineBuf.buf)
-		s = strings.TrimSpace(s)
-		next, ok := pc.Actions[s]
-		if ok {
-			next.Activate(ses)
-		} else {
-			pc.Activate(ses)
-		}
+
+	line := ses.GetLine()
+	line = strings.Trim(line, " \t\r\n")
+	aNum, err := strconv.Atoi(line)
+	if err == nil {
+		// It was a number.
+		card, ok := Cards[aNum]
+		card.Activate(ses)
+	} else {
+		// It was something else.
+		log.Panicf("Not a number: %q", line)
 	}
 }
+*/
 
 type ActionCard struct {
 	Func func(ses *Session)
@@ -189,17 +214,91 @@ var RunNitros9Card = &ActionCard{
 	},
 }
 
-var HomeCard = &Card{
-	Prompt: `Welcome to Lemma.
-  1) run nitros9
-  2) run nyancat
-  3) login user
-  4) create user
+func Add(num int, parent int, name string, tc *TCard) {
+	tc.Num = num
+	tc.name = name
+	tc.Moms = append(tc.Moms, parent)
+	tc.Kids = make(map[int]Card)
+
+	Cards[num] = tc
+	if num > 0 {
+		for _, mom := range tc.Moms {
+			p, ok := Cards[mom]
+			if !ok {
+				log.Panicf("Add TCard #%d=%q: cannot find mom %d", num, name, mom)
+			}
+			ptc, ok := p.(*TCard)
+			if !ok {
+				log.Panicf("Add TCard #%d=%q: mom not a TCard %T :: %v", num, name, p, p)
+			}
+			ptc.Kids[num] = tc
+		}
+	}
+}
+
+func init() {
+	Add(0, 0, "Home", &TCard{
+		Text: `Welcome to Lemma.
+This is the home card.
+You can return here by typing 0.
 `,
-	Actions: map[string]*Card{
-		{"1", RunNitros9Card},
-		{"2", RunNitros9Card},
-		{"3", RunNitros9Card},
-		{"4", RunNitros9Card},
-	},
+	})
+
+	Add(10, 0, "CocoIO-Tests", &TCard{
+		Text: `These are some tests you can try
+to stress your CocoIO Card.
+`,
+	})
+
+	Add(20, 0, "Demos", &TCard{})
+
+	Add(30, 0, "Nitros-9", &TCard{})
+	Add(31, 30, "Level1", &TCard{})
+	Add(32, 30, "Level2", &TCard{})
+	Add(33, 30, "EOU", &TCard{})
+}
+
+func Bold(s string) string {
+	var bb bytes.Buffer
+	for _, r := range s {
+		bb.WriteByte(byte(r))
+		bb.WriteByte(1)
+	}
+	return bb.String()
+}
+
+func BoldInt(x int) string {
+	return Bold(fmt.Sprintf("%d", x))
+}
+
+func Run(ses *Session) {
+	current := Cards[0].(*TCard)
+
+	for {
+		ses.Screen.Clear()
+		ses.Screen.PutStr(fmt.Sprintf("%d = %q\n\n", BoldInt(current.Num), current.name))
+		ses.Screen.PutStr(current.Text + "\n")
+		for num, kid := range current.Kids {
+			ses.Screen.PutStr(fmt.Sprintf("[%d] %s\n", num, kid.Name()))
+		}
+		ses.Screen.Push(ses)
+
+		line := ses.GetLine()
+		line = strings.Trim(line, " \t\r\n")
+		aNum, err := strconv.Atoi(line)
+		if err == nil {
+			// It was a number.
+			card, ok := Cards[aNum]
+			if ok {
+				tcard, ok := card.(*TCard)
+				if ok {
+					current = tcard
+				}
+			} else {
+			}
+		} else {
+			// It was something else.
+			log.Panicf("Not a number: %q", line)
+		}
+	}
 }
