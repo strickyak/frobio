41e69bdbd1df1fba4a1559e323d4a98fce4d3109
strick <strick@yak.net>
2023-06-27 22:40:22 -0700

    makes two lemmings; untested

---
+++ frob3/lemmings/Nitros9_Coco3_H6309_Level2.go
@@ -0,0 +1,39 @@
+package main
+
+var Nitros9_Coco3_H6309_Level2_Cf = &Os9ConfigForLemma{
+	Name:  "Nitros9_Coco3_H6309_Level2",
+	Level: "level2",
+	Port:  "coco3_6309",
+	Boot1Mods: []string{
+		"rel_80", "boot_lemma", "krn",
+	},
+	Boot2Mods: []string{
+		"krnp2",
+		"ioman",
+		"init",
+		"rbf.mn",
+		"scf.mn",
+		"vtio.dr",
+		"keydrv_cc3.sb",
+		"snddrv_cc3.sb",
+		"joydrv_joy.sb",
+		"cowin.io",
+		"term_win80.dt",
+
+		"w.dw",
+		"w1.dw",
+		"w2.dw",
+		"w3.dw",
+		"w4.dw",
+		"w5.dw",
+		"w6.dw",
+		"w7.dw",
+
+		"clock_60hz",
+		"clock2_soft",
+		"sysgo_dd",
+
+		"./rblemma",
+		"./dd.b0",
+	},
+}
---
+++ frob3/lemmings/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Os9ConfigForLemma struct {
+	Name      string
+	Level     string
+	Port      string
+	Boot1Mods []string
+	Boot2Mods []string
+}
+
+func (cf *Os9ConfigForLemma) SlurpBoots() (b1 []byte, b2 []byte) {
+	for _, filename := range cf.Boot1Mods {
+		b1 = append(b1, SlurpFilename(cf, filename)...)
+	}
+	for _, filename := range cf.Boot2Mods {
+		b2 = append(b2, SlurpFilename(cf, filename)...)
+	}
+	return b1, b2
+}
+
+var nitros9dirFlag = flag.String("nitros9dir", "", "root of nitros9 sources")
+
+func Quint(cmd byte, n int, p int) []byte {
+	return []byte{
+		cmd,
+		byte(n >> 8),
+		byte(n),
+		byte(p >> 8),
+		byte(p)}
+}
+
+func ChunksOf(bb []byte) [][]byte {
+	var z [][]byte
+	for len(bb) > 0 {
+		var n int
+		if len(bb) < 1024 {
+			n = 1024
+		} else {
+			n = len(bb)
+		}
+		z = append(z, bb[:n])
+		bb = bb[n:]
+	}
+	return z
+}
+
+func SlurpFilename(cf *Os9ConfigForLemma, filename string) []byte {
+	if strings.HasPrefix(filename, "./") {
+		filename = filepath.Join("results/MODULES/", filename)
+	} else {
+		filename = filepath.Join(*nitros9dirFlag, cf.Level, cf.Port, "modules", filename)
+	}
+	bb, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Panicf("Cannot read filename %q: %v", filename, err)
+	}
+	return bb
+}
+
+func Emit(w io.Writer, bb []byte) {
+	cc, err := w.Write(bb)
+	if err != nil {
+		log.Panicf("Cannot write %d bytes to file: %v", len(bb), err)
+	}
+	if cc != len(bb) {
+		log.Panicf("Short write of %d bytes to file", len(bb))
+	}
+}
+
+func main() {
+	flag.Parse()
+	if *nitros9dirFlag == "" {
+		dir := os.Getenv("NITROS9DIR")
+		if dir == "" {
+			log.Panicf("You must define the --nitros9dir flag or the NITROS9DIR environment.")
+		}
+		nitros9dirFlag = &dir
+	}
+	BuildConfig(Nitros9_Coco3_M6809_Level2_Cf)
+	BuildConfig(Nitros9_Coco3_H6309_Level2_Cf)
+}
+
+func BuildConfig(cf *Os9ConfigForLemma) {
+	w, err := os.Create(cf.Name + ".lem")
+	if err != nil {
+		log.Panicf("Cannot create %q: %v", cf.Name, err)
+	}
+	defer w.Close()
+
+	b1, b2 := cf.SlurpBoots()
+
+	// Set interrupt vectors.
+	Emit(w, Quint(0, 16, 0xFFF0))
+	Emit(w, SlurpFilename(cf, "vectors"))
+
+	// Load binary b1 at $2600.
+	Emit(w, Quint(0, len(b1), 0x2600))
+	Emit(w, b1)
+
+	// Launch binary b1.
+	Emit(w, Quint(255, 0, 0x2602))
+
+	// Send b2: 211 begin, 212 chunks, 213 end.
+	Emit(w, Quint(211, len(b2), 0))
+	for _, chunk := range ChunksOf(b2) {
+		Emit(w, Quint(212, len(chunk), 0))
+		Emit(w, chunk)
+	}
+	Emit(w, Quint(213, len(b2), 0))
+}
