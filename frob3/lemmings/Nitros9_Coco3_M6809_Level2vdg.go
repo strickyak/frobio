6b6a423b023889d94f9b49731c580e8299c37573
strick <strick@yak.net>
2023-10-05 00:30:06 -0700

    VDG and 40_col lemmings for smaller Level 2

---
+++ frob3/lemmings/Nitros9_Coco3_M6809_Level2vdg.go
@@ -0,0 +1,55 @@
+package main
+
+func init() {
+	Os9(&Os9ConfigForLemma{
+		Name:        "Nitros9_Coco3_M6809_Level2vdg",
+		Level:       "level2",
+		Port:        "coco3",
+		DefaultDisk: "nitros9/level2/coco3/NOS9_6809_L2_cocosdc.dsk",
+		Boot1Mods: []string{
+			"rel_80", "./boot.lemma", "krn",
+		},
+		Boot2Mods: []string{
+			"krnp2",
+			"ioman",
+			"init",
+			"rbf.mn",
+			"scf.mn",
+			"vtio.dr",
+			"keydrv_cc3.sb",
+			"snddrv_cc3.sb",
+			"joydrv_joy.sb",
+			"covdg.io", // "cowin.io",
+			"term_vdg.dt", // "term_win80.dt",
+
+			"w.dw",
+			"w1.dw",
+			"w2.dw",
+			"w3.dw",
+			// "w4.dw",
+			// "w5.dw",
+			// "w6.dw",
+			// "w7.dw",
+
+			"clock_60hz",
+			"clock2_soft",
+			"sysgo_dd",
+
+			// "pipeman.mn",
+			// "piper.dr",
+			// "pipe.dd",
+
+			// "rbsuper.dr",
+			// "llcocosdc.dr",
+			// "sd0_cocosdc.dd",
+			// "sd1_cocosdc.dd",
+
+			// "./fuseman",
+			// "./fuser",
+			// "./fuse",
+
+			"./rblemma",
+			"./dd.b0",
+		},
+	})
+}
