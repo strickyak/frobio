f34d633d5f7b7d7079eed9247eb86f997c91a203
strick <strick@yak.net>
2023-11-29 00:23:46 -0800

    treenet.go plans.go

---
+++ frob3/lemma/lib/plans.go
@@ -0,0 +1,42 @@
+/*
+Plan for a Secret:
+  Client presents (Hostname, Mac) as a Principal (and might request authentication/encryption).
+  If Client has registered a secret with Server,
+    Server an offer a Challenge Nonce.
+    Client computes Hash(Secret+Challenge), and replies with it.
+    Server replies "+ok." or "-bad".
+    Thereafter, if OK,
+      Client sends with "Enc(Hash(Secret+Challenge+"client"))"
+      Server sends with "Enc(Hash(Secret+Challenge+"server"))"
+
+
+Client should speak first (to confuse random knockers):
+  (1, n<256, 0x0101=c_vsn): "host=XYZZY,mac=123456789a,flags=010000,rom1=4444,fanout=6" "proto=crkey1,enc=arc49"
+Server replies:
+  (1, n<256, 0x0101=s_vsn): "+ok host=SFO,mac=1111111151,z=16.129+7" "proto=crkey1,enc=arc49,1=763876213862181"
+                            "chain=MID,HUB"
+Both will use MIN(c_vsn, s_vsn) thereafter.
+
+Perhaps a (2) line for introducing or deleting a parent/kid.
+
+If challenged, client needs to respond:
+  (1, n<256, 0x0101=c_vsn): "2=743274983274932798327932".
+And server can reply
+  (1, n<256, 0x0101=s_vsn): "+ok u=strick,g=cocoio+frobio+f256" | "-bad"
+
+Thereafter, either side can initiate.
+To keep channels unique:
+  Client-initated channels even.
+  Server-initated channels odd.
+
+
+TODO:
+  /b1: per-user       ( at peer )
+  /b2: public sharing ( at peer )
+  /b3: per-user       ( at top )
+  /b4: public sharing ( at top )
+  /r/HOST/:   Remote General FS
+
+*/
+
+package lib
---
+++ frob3/lemma/lib/treenet.go
@@ -0,0 +1,70 @@
+package lib
+
+import (
+	"flag"
+)
+
+var KIDSPORT = flag.Int("kidsport", 20185, "Lemma kids can connect here (for treenet)")
+var PARENT = flag.String("parent", "", "Connect to a parent lemma host:port (for treenet)")
+var NAME = flag.String("name", "", "Name to use (for treenet)")
+
+var KnownNodes map[string]*TreeNode // closure Parents and immediate Kids
+
+type TreeNode struct {
+	Serial  int
+	Version int
+	Flags   []byte
+	Host    string
+	Mac     [5]byte
+	Secret  []byte
+
+	Parent *TreeNode
+	Kids   map[string]*TreeNode
+
+	User   string
+	Groups []string
+	Where  *TreeNode // which host authenticated User & Group namespace
+
+	FanMax    int
+	ZeroNet   int
+	ZeroSlots int
+
+	// Encrypt *Encryption
+	// First   []byte // e.g. the Challenge
+	// Second  []byte // e.g. the Reply
+}
+
+/*
+func TreeListen() {
+	l, err := net.Listen("tcp", Format(":%d", *KIDSPORT))
+	if err != nil {
+		log.Panicf("Tree: Cannot Listen(): %v", err)
+	}
+	defer l.Close()
+	log.Printf("Tree: Listening on port %d", *PORT)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Panicf("Tree: Cannot Accept() connection: %v", err)
+		}
+		log.Printf("Tree: Accepted %q.", conn.RemoteAddr().String())
+		go TreeServe(conn)
+	}
+}
+
+func TreeServe(conn net.Conn) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Printf("Tree: Closing connection %q: Exception %v", conn.RemoteAddr().String(), r)
+		} else {
+			log.Printf("Tree: Done with connection %q", conn.RemoteAddr().String())
+		}
+		conn.Close()
+	}()
+
+  var q Quint
+
+TODO
+*/
