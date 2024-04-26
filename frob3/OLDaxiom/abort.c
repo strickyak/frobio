e1cb91a46ed9c18f98f86521286ef7e4a9c2c903
Henry Strickland <strick@yak.net>
2024-04-26 02:36:04 -0700

    Delete OLDaxiom

--- frob3/OLDaxiom/abort.c
+++
@@ -1,8 +0,0 @@
-void abort() {
-  // Assume text screen is still at 0x0400,
-  // and splash the middle of it with Xs.
-  for (int p=0x04C0; p < 0x0540; p++) {
-    *(char*)p = 'X';
-  }
-  while (1) {}
-}
