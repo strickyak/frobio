de159a659c6478307b15c6ace2f70a15041ce9b0
Henry Strickland <strick@yak.net>
2024-05-15 22:03:12 -0700

    partial work on lemmings/Rogue512.go

---
+++ frob3/lemmings/Rogue512.go
@@ -0,0 +1,41 @@
+package main
+/* FIXME?
+
+func init() {
+	Os9(&Os9ConfigForLemma{
+		Name:        "Rogue512",
+		Level:       "level2",
+		Port:        "coco3",
+		DefaultDisk: "/home/strick/NEW/R1/ROGUE512.DSK",
+		// DefaultDisk: "../eou-m6809/68SDC.VHD",
+		Boot1Base:   "/home/strick/NEW/R1/ROGUE512.DSK",
+		// Boot1Base:   "eou-m6809/68EMU.dsk",
+		Boot1Mods: []string{
+			"_HEAD_",       // Special mark to keep the Base's head.
+			"./boot.lemma", // Insert our own boot.lemma file.
+			"_TAIL_",       // Special mark to keep the Base's tail.
+		},
+		Boot2Base: "/home/strick/NEW/R1/ROGUE512.DSK",
+		// Boot2Base: "eou-m6809/68EMU.dsk",
+		Boot2Mods: []string{
+	"<OS9p2",
+	"<Init",
+	"<IOMan",
+	"<RBF",
+	// "<CC3Disk",
+	// "<D0",
+	// "<D1",
+	// "<DD",
+			"./rblemma",
+			"./dd.b0",
+	"<SCF",
+	"<CC3IO",
+	"<GrfInt",
+	"<Term",
+	"<Clock",
+	"<CC3Go",
+		},
+	})
+}
+
+*/
