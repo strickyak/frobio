1c988af8cc2a4a0bedaa811c2828f5bf99080ab1
Henry Strickland <strick@yak.net>
2025-12-29 00:37:53 -0500

    add -j2 to assimilator makefile

--- frob3/assimilator/30-build.bash
+++ frob3/assimilator/30-build.bash
@@ -24,7 +24,7 @@ ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -n root@$H '
 		test -d coco-shelf || git clone https://github.com/strickyak/coco-shelf.git
 
 		cd coco-shelf
-		time make ANON=1 KEEP=1 2>&1 | tee /tmp/make.log
+		time make -j2 ANON=1 KEEP=1 2>&1 | tee /tmp/make.log
 
 		cd build-frobio
 		make tarballs
